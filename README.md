73d3b61a94c2d6bb9220d90a2cab0beaf862bec3
Carlos Pizarro <carlospizarromorales@gmail.com>
2023-04-23 00:14:24 -0400

    Revisión entrete del title.js

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # gato-y-otros
 
+### EDIT:  
+
+    este es un préstamo del estimado Sansón. Yo solo lo utilicé porque encuentro bacán el efecto en el document.title
+
+
 Este es un proyecto de un juego de Gato (tik tak toe) en JavaScript manipulando el HTML y el CSS.
 El estilo visual no es lo primordial ya que obviamente se puede mejorar.
+
+----
+
+Enlace al [repo](https://github.com/RicardoGallegos92/gato-Dragon-Ball)
+
+
+Enlace al perfil de [Sansón](https://github.com/RicardoGallegos92/gato-Dragon-Ball)
--- js/title.js
+++ js/title.js
@@ -1,12 +1,13 @@
-let titulo = "The Z Warriors";
+/**
+let titulo = "The Z Warriors";  
 let titulo_clon;
-letra = '...........................';
-iteracion = -1;
+let letra = '...........................';
+let iteracion = -1;
 document.title = titulo;
-largo_titulo = titulo.length;
+let largo_titulo = titulo.length;
 document.title = titulo;
 var time_shooter = null; //por los lol-es
-retraso_ms = 500; //espacio de tiempo entre cambio de letras en milisegundos
+let retraso_ms = 500; //espacio de tiempo entre cambio de letras en milisegundos
 
 function mover_titulo(){
 	if(iteracion < largo_titulo){
@@ -21,3 +22,44 @@ function mover_titulo(){
 };
 
 window.addEventListener('load', mover_titulo)
+**/
+
+/**********************************************
+**               OTRA VERSION                **
+**********************************************/
+// window.addEventListener('load', mover_titulo);
+
+const titulo = "The Z Warriors";
+let titulo_mutable = titulo;  
+document.title = titulo_mutable;
+
+function mover_titulo(){
+  const titleArray = titulo_mutable.split('');
+  if (titleArray.every(e => e === '.')) {
+    document.title = titulo_mutable = titulo;
+    return;
+  }
+
+  for (let i in titleArray) {
+    if ( titleArray[i] === '.' ) continue;
+    titleArray[i] = '.';
+    break;
+  }
+
+  document.title = titulo_mutable = titleArray.join('');
+};
+
+/**********************************************
+ **                 VERSION 2                 **
+ **********************************************/
+function mover_titulo2(){
+  if (!(titulo_mutable.match(/[a-z ]/i))) return document.title = titulo_mutable = titulo;
+  
+  const regex = /[a-z ]/i;
+  document.title = titulo_mutable = titulo_mutable.replace(regex, '.');
+}
+
+/**********************************************
+**                Y...ACCIÓN                 **
+**********************************************/
+const id = setInterval(mover_titulo2, 1000);
